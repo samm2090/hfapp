e19d723124208a6e9937b812d7503ed2fbac2c27
Sergio Muroy <muroy.sergio@gmail.com>
2017-03-02 16:56:36 -0500

    login fragment 5

---
+++ app/src/main/java/com/hazfutbol/hfapp/fragments/Register5Fragment.java
@@ -0,0 +1,67 @@
+package com.hazfutbol.hfapp.fragments;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.hazfutbol.hfapp.R;
+
+/**
+ * Created by ninosimeon on 09/02/17.
+ */
+
+public class Register5Fragment extends Fragment {
+
+    Button btnUploadPhoto;
+    ImageView profileImage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register4,container,false);
+        btnUploadPhoto = (Button) view.findViewById(R.id.btnUploadPhoto);
+        profileImage = (ImageView) view.findViewById(R.id.profile_image);
+
+        btnUploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, 1);
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+            Uri selectedImage = data.getData();
+
+            profileImage.setImageURI(selectedImage);
+
+        }
+    }
+}
